3d70a90df27f56826041c26bc98d2a1077005e7d
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-09-17 17:53:15 +0200

    `build_docs.sh` uses variables rather than hardcoded values
    The script wasn't really set up to be used by other people, for instance
    the location (and I'm assuming you are like me and don't have your SDK
    located in the same identical place as Adam Saltsman does)
    
    The script now uses variables wihich you can modify to suit your needs.

--- utils/build_docs.sh
+++ utils/build_docs.sh
@@ -1,2 +1,9 @@
 #! /bin/sh
-/Applications/Adobe\ Flash\ Builder\ 4\ Plug-in/sdks/4.0.0/bin/asdoc -source-path . -doc-sources . -output docs
+
+# NOTE: These variables are relative to where you are calling the script from,
+#	NOT the location of the script!
+FLEX_SDK_LOCATION="/Applications/Adobe Flash Builder 4 Plug-in/sdks/4.0.0"
+CODE_LOCATION="."
+TARGET_LOCATION="docs"
+
+"$FLEX_SDK_LOCATION"/bin/asdoc -source-path "$CODE_LOCATION" -doc-sources "$CODE_LOCATION" -output "$TARGET_LOCATION"
